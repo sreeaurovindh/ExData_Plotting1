b32bd216c254efa73a7da6474b655858e2d2aac1
sreeaurovindh <sreeaurovindh@gmail.com>
2014-05-11 16:06:14 -0700

    Changes to Plot 3

--- plot3.R
+++ plot3.R
@@ -1,24 +1,28 @@
-plot3<- function(){
-  # Renamed the source file as hhcomp.txt and then loaded the whole file
-  home<-read.csv("hhcomp.txt",sep=";")
-  # Conversion to date format 
-  home[,1]<-as.Date(home[,1],"%d/%m/%Y")
-  # select the subset of dates required from the global set
-  dataset<-home[home[,1]>='2007-02-01' & home[,1] <= '2007-02-02',]
-  # Convert each into numeric components
-  dataset$Sub_metering_1 <- as.numeric(dataset$Sub_metering_1)
-  dataset$Sub_metering_2 <- as.numeric(dataset$Sub_metering_2)
-  dataset$Sub_metering_3 <- as.numeric(dataset$Sub_metering_3)
- 
-  # Create a PNG Image of size
-  png("plot3.png",width=480,height=480,bg="transparent")
-  # create a plot
-  with(dataset,plot(as.numeric(dataset$Sub_metering_1),type="n",xaxt="n",xlab="",ylab="Energy sub Metering"))
-  lines(dataset$Sub_metering_1,col="black")
-  lines(dataset$Sub_metering_2,col="red")
-  lines(dataset$Sub_metering_3,col="blue")
-  #Since there are 1440 instances of both thursday and friday we have got a scale starting from 0, 1440 and then to 2880
-  axis(1,at=c(0,1440,2880),lab=c("Thu","Fri","Sat"))
-  legend("topright", lty=1, col = c("black", "red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
-  dev.off()
+plot3<-function(){
+#Read in all raw data
+rawData <- read.table("hhcomp.txt", header=F, sep=";", colClasses = "character")
+# Retain only the dates required
+dataset <- rawData[grepl("^[12]/2/2007", rawData[,1]),]
+# Bind the "Date" and "Time" columns to a single "Date/Time" column
+dataset <- cbind(strptime(paste(dataset[,1], dataset[,2]), format="%d/%m/%Y %H:%M:%S"), dataset[,-c(1,2)])
+# Set the column headers
+names(dataset) <- c("Date/Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+# Open "PNG" device
+png(filename="plot3.png", width=480, height=480)
+# Prepare the plot, but do not add any points
+plot(dataset$Date, as.numeric(dataset$Sub_metering_1), pch=NA, main="", ylab = "Energy sub metering", xlab = "")
+# Connect the dots: black is station 1, red is 2, blue is 3
+lines(dataset$Date, as.numeric(dataset$Sub_metering_1), col="black")
+lines(dataset$Date, as.numeric(dataset$Sub_metering_2), col="red")
+lines(dataset$Date, as.numeric(dataset$Sub_metering_3), col="blue")
+# Add a legend
+legend("topright",
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=c(1,1,1),
+       lwd=c(1,1,1),
+       col=c("black", "red", "blue"))
+
+# Close the "PNG" device
+dev.off()
 }
